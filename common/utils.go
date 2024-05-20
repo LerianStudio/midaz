09c622b908989bd334fab244e3639f312ca1b0df
MartinezAvellan <martinez.avellan@hotmail.com>
2024-05-20 18:09:19 -0300

    feature: create two methods, validate type and validate currency validate ISO 4217 :bug:

--- common/utils.go
+++ common/utils.go
@@ -76,3 +76,49 @@ func ValidateCountryAddress(country string) error {
 
 	return nil
 }
+
+// ValidateType validate type values if c
+func ValidateType(t string) error {
+	types := []string{"crypto", "currency", "commodity", "others"}
+
+	if !slices.Contains(types, t) {
+		return ValidationError{
+			Code:    "0033",
+			Title:   "Invalid Code Format",
+			Message: "The 'code' field must be alphanumeric, in upper case, and must contain at least one letter. Please provide a valid code.",
+		}
+	}
+
+	return nil
+}
+
+// ValidateCurrency validate if code contains in currencies list using ISO 4217
+func ValidateCurrency(code string) error {
+	currencies := []string{
+		"AFN", "ALL", "DZD", "USD", "EUR", "AOA", "XCD", "XCD", "ARS", "AMD", "AWG", "AUD", "EUR", "AZN", "BSD", "BHD",
+		"BDT", "BBD", "BYN", "EUR", "BZD", "XOF", "BMD", "BTN", "INR", "BOB", "BOV", "USD", "BAM", "BWP", "NOK", "BRL", "USD", "BND", "BGN",
+		"XOF", "BIF", "CVE", "KHR", "XAF", "CAD", "KYD", "XAF", "XAF", "CLF", "CLP", "CNY", "AUD", "AUD", "COP", "COU", "KMF", "CDF", "XAF",
+		"NZD", "CRC", "EUR", "CUC", "CUP", "ANG", "EUR", "CZK", "XOF", "DKK", "DJF", "XCD", "DOP", "USD", "EGP", "SVC", "USD", "XAF", "ERN",
+		"EUR", "ETB", "EUR", "FKP", "DKK", "FJD", "EUR", "EUR", "EUR", "XPF", "EUR", "XAF", "GMD", "GEL", "EUR", "GHS", "GIP", "EUR", "DKK",
+		"XCD", "EUR", "USD", "GTQ", "GBP", "GNF", "XOF", "GYD", "HTG", "USD", "AUD", "EUR", "HNL", "HKD", "HUF", "ISK", "INR", "IDR", "XDR",
+		"IRR", "IQD", "EUR", "GBP", "ILS", "EUR", "JMD", "JPY", "GBP", "JOD", "KZT", "KES", "AUD", "KPW", "KRW", "KWD", "KGS", "LAK", "EUR",
+		"LBP", "LSL", "ZAR", "LRD", "LYD", "CHF", "EUR", "EUR", "MOP", "MGA", "MWK", "MYR", "MVR", "XOF", "EUR", "USD", "EUR", "MRU", "MUR",
+		"EUR", "XUA", "MXN", "MXV", "USD", "MDL", "EUR", "MNT", "EUR", "XCD", "MAD", "MZN", "MMK", "NAD", "ZAR", "AUD", "NPR", "EUR", "XPF",
+		"NZD", "NIO", "XOF", "NGN", "NZD", "AUD", "USD", "NOK", "OMR", "PKR", "USD", "PAB", "USD", "PGK", "PYG", "PEN", "PHP", "NZD", "PLN",
+		"EUR", "USD", "QAR", "MKD", "RON", "RUB", "RWF", "EUR", "EUR", "SHP", "XCD", "XCD", "EUR", "EUR", "XCD", "WST", "EUR", "STN", "SAR",
+		"XOF", "RSD", "SCR", "SLE", "SGD", "ANG", "XSU", "EUR", "EUR", "SBD", "SOS", "ZAR", "SSP", "EUR", "LKR", "SDG", "SRD", "NOK", "SZL",
+		"SEK", "CHE", "CHF", "CHW", "SYP", "TWD", "TJS", "TZS", "THB", "USD", "XOF", "NZD", "TOP", "TTD", "TND", "TRY", "TMT", "USD", "AUD",
+		"UGX", "UAH", "AED", "GBP", "USD", "USD", "USN", "UYI", "UYU", "UZS", "VUV", "VEF", "VED", "VND", "USD", "USD", "XPF", "MAD", "YER",
+		"ZMW", "ZWL", "EUR",
+	}
+
+	if !slices.Contains(currencies, code) {
+		return ValidationError{
+			Code:    "0005",
+			Title:   "Invalid Data provided.",
+			Message: "Invalid Data provided.",
+		}
+	}
+
+	return nil
+}
