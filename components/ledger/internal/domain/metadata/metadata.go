d68be08765d57c7c01d4a9b1f0466070007839c2
MartinezAvellan <martinez.avellan@hotmail.com>
2024-06-04 17:20:03 +0200

    fix: bring back omitempty on metadata in field _id because cant generate automatic id without :bug:

--- components/ledger/internal/domain/metadata/metadata.go
+++ components/ledger/internal/domain/metadata/metadata.go
@@ -11,7 +11,7 @@ import (
 
 // MetadataMongoDBModel represents the metadata into mongodb context
 type MetadataMongoDBModel struct {
-	ID         primitive.ObjectID `bson:"_id"`
+	ID         primitive.ObjectID `bson:"_id,omitempty"`
 	EntityID   string             `bson:"entity_id"`
 	EntityName string             `bson:"entity_name"`
 	Data       JSON               `bson:"metadata"`
