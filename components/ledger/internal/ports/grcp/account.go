2679e9bfe2d94fcc201e5672cec1f86feca5eb95
MartinezAvellan <martinez.avellan@hotmail.com>
2024-06-11 17:57:30 +0200

    fix: adjust grpc account service :bug:

--- components/ledger/internal/ports/grcp/account.go
+++ components/ledger/internal/ports/grcp/account.go
@@ -2,30 +2,45 @@ package grcp
 
 import (
 	"context"
+	"github.com/google/uuid"
 
 	proto "github.com/LerianStudio/midaz/components/ledger/proto/account"
 )
 
-type Account struct {
+type AccountService struct {
 	proto.UnimplementedAccountServiceServer
 }
 
-func NewAccount() *Account {
-	return &Account{}
+func NewAccountService() *AccountService {
+	return &AccountService{}
 }
 
-func (account *Account) GetByIds(ctx context.Context, ids *proto.ManyAccountsID) (*proto.ManyAccountsResponse, error) {
+func (account *AccountService) GetByIds(ctx context.Context, ids *proto.ManyAccountsID) (*proto.ManyAccountsResponse, error) {
+
+	a := proto.Account{
+		ID:    uuid.NewString(),
+		Alias: "Teste",
+	}
+
+	var accounts []*proto.Account
+
+	accounts = append(accounts, &a)
+
+	response := proto.ManyAccountsResponse{
+		Accounts: accounts,
+	}
+
+	return &response, nil
+}
+
+func (account *AccountService) GetByAlias(ctx context.Context, alias *proto.ManyAccountsAlias) (*proto.ManyAccountsResponse, error) {
 	return nil, nil
 }
 
-func (account *Account) GetByAlias(ctx context.Context, alias *proto.ManyAccountsAlias) (*proto.ManyAccountsResponse, error) {
+func (account *AccountService) Update(ctx context.Context, update *proto.UpdateRequest) (*proto.Account, error) {
 	return nil, nil
 }
 
-func (account *Account) Update(ctx context.Context, update *proto.UpdateRequest) (*proto.Account, error) {
-	return nil, nil
-}
-
-func (account *Account) GetByFilters(ctx context.Context, filter *proto.GetByFiltersRequest) (*proto.ManyAccountsResponse, error) {
+func (account *AccountService) GetByFilters(ctx context.Context, filter *proto.GetByFiltersRequest) (*proto.ManyAccountsResponse, error) {
 	return nil, nil
 }
