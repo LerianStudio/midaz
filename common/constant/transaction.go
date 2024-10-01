4f5a03b920961e33a76d96ead2c05500f97020f8
MartinezAvellan <martinez.avellan@hotmail.com>
2024-10-01 19:59:02 +0200

    feat: create transaction constant :sparkles:

---
+++ common/constant/transaction.go
@@ -0,0 +1,10 @@
+package constant
+
+const (
+	CREATED      = "CREATED"
+	APPROVED     = "APPROVED"
+	PRE_APPROVED = "PRE_APPROVED"
+	SENT         = "SENT"
+	CANCELED     = "CANCELED"
+	DECLINED     = "DECLINED"
+)
