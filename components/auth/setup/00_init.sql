dc97b65f00c96a2095e08a30da9b856f30316b99
Brecci <brecci@lerian.studio>
2024-10-03 14:25:49 -0300

    refactor: remove domain from casdoor and casbin policies :hammer:

--- components/auth/setup/00_init.sql
+++ components/auth/setup/00_init.sql
@@ -14,26 +14,26 @@ CREATE TABLE IF NOT EXISTS "casbin_lerian_rule" (
 );
 
 -- Create indexes for the table
-CREATE INDEX IF NOT EXISTS idx_v4 ON "casbin_lerian_rule" ("v4");
-CREATE INDEX IF NOT EXISTS idx_v5 ON "casbin_lerian_rule" ("v5");
 CREATE INDEX IF NOT EXISTS idx_ptype ON "casbin_lerian_rule" ("ptype");
 CREATE INDEX IF NOT EXISTS idx_v0 ON "casbin_lerian_rule" ("v0");
 CREATE INDEX IF NOT EXISTS idx_v1 ON "casbin_lerian_rule" ("v1");
 CREATE INDEX IF NOT EXISTS idx_v2 ON "casbin_lerian_rule" ("v2");
 CREATE INDEX IF NOT EXISTS idx_v3 ON "casbin_lerian_rule" ("v3");
+CREATE INDEX IF NOT EXISTS idx_v4 ON "casbin_lerian_rule" ("v4");
+CREATE INDEX IF NOT EXISTS idx_v5 ON "casbin_lerian_rule" ("v5");
 
 -- Insert the default group and policy
 INSERT INTO "casbin_lerian_rule" ("id", "ptype", "v0", "v1", "v2", "v3", "v4", "v5") VALUES
-(1,	'g',	'user_john',	'admin_role',	'app-midaz',	'',	'',	''),
-(2,	'g',	'user_kate',	'admin_role',	'app-midaz',	'',	'',	''),
-(3,	'g',	'user_lisa',	'admin_role',	'app-midaz',	'',	'',	''),
-(4,	'g',	'user_john',	'developer_role',	'app-midaz',	'',	'',	''),
-(5,	'g',	'user_kate',	'developer_role',	'app-midaz',	'',	'',	''),
-(6,	'g',	'user_bob',	'developer_role',	'app-midaz',	'',	'',	''),
-(7,	'g',	'user_mike',	'user_role',	'app-midaz',	'',	'',	''),
-(8,	'p',	'admin_role',	'app-midaz',	'*',	'*',	'',	''),
-(9,	'p',	'developer_role',	'app-midaz',	'*',	'POST',	'',	''),
-(10,	'p',	'developer_role',	'app-midaz',	'*',	'GET',	'',	''),
-(12,	'p',	'developer_role',	'app-midaz',	'*',	'PATCH',	'',	''),
-(11,	'p',	'developer_role',	'app-midaz',	'*',	'PUT',	'',	''),
-(13,	'p',	'user_role',	'app-midaz',	'*',	'GET',	'',	'');
+(1,	'g',	'user_john',	'admin_role',	'',	'',	'',	''),
+(2,	'g',	'user_kate',	'admin_role',	'',	'',	'',	''),
+(3,	'g',	'user_lisa',	'admin_role',	'',	'',	'',	''),
+(4,	'g',	'user_john',	'developer_role',	'',	'',	'',	''),
+(5,	'g',	'user_kate',	'developer_role',	'',	'',	'',	''),
+(6,	'g',	'user_bob',	'developer_role',	'',	'',	'',	''),
+(7,	'g',	'user_mike',	'user_role',	'',	'',	'',	''),
+(8,	'p',	'admin_role',	'*',	'*',	'',	'',	''),
+(9,	'p',	'developer_role',	'*',	'POST',	'',	'',	''),
+(10,	'p',	'developer_role',	'*',	'GET',	'',	'',	''),
+(12,	'p',	'developer_role',	'*',	'PATCH',	'',	'',	''),
+(11,	'p',	'developer_role',	'*',	'PUT',	'',	'',	''),
+(13,	'p',	'user_role',	'*',	'GET',	'',	'',	'');
