583890a6c1d178b95b41666a91600a60d3053123
MartinezAvellan <martinez.avellan@hotmail.com>
2024-10-18 13:30:36 +0200

    fix: adjust to validate if exists code on assets; :bug:

--- components/ledger/internal/app/command/create-account.go
+++ components/ledger/internal/app/command/create-account.go
@@ -50,12 +50,7 @@ func (uc *UseCase) CreateAccount(ctx context.Context, organizationID, ledgerID, 
 		return nil, common.ValidateBusinessError(cn.ErrInvalidCodeFormat, reflect.TypeOf(a.Account{}).Name())
 	}
 
-	isAsset, err := uc.AssetRepo.FindByNameOrCode(ctx, uuid.MustParse(organizationID), uuid.MustParse(ledgerID), "", cai.AssetCode)
-	if err != nil {
-		logger.Errorf("Error to find asset by asset code: %v", err.Error())
-		return nil, err
-	}
-
+	isAsset, _ := uc.AssetRepo.FindByNameOrCode(ctx, uuid.MustParse(organizationID), uuid.MustParse(ledgerID), "", cai.AssetCode)
 	if !isAsset {
 		return nil, common.ValidateBusinessError(cn.ErrAssetCodeNotFound, reflect.TypeOf(a.Account{}).Name())
 	}
