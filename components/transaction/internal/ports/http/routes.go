5e4519f6e64e65b094bfff685e4ac0221e3183c7
Andre Russowsky <andrerussowsky@hotmail.com>
2024-10-23 14:58:22 -0300

    feat: Create asset rates :sparkles:

--- components/transaction/internal/ports/http/routes.go
+++ components/transaction/internal/ports/http/routes.go
@@ -4,13 +4,14 @@ import (
 	"github.com/LerianStudio/midaz/common/mcasdoor"
 	"github.com/LerianStudio/midaz/common/mlog"
 	lib "github.com/LerianStudio/midaz/common/net/http"
+	a "github.com/LerianStudio/midaz/components/transaction/internal/domain/assetrate"
 	o "github.com/LerianStudio/midaz/components/transaction/internal/domain/operation"
 	t "github.com/LerianStudio/midaz/components/transaction/internal/domain/transaction"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
-func NewRouter(lg mlog.Logger, cc *mcasdoor.CasdoorConnection, th *TransactionHandler, oh *OperationHandler) *fiber.App {
+func NewRouter(lg mlog.Logger, cc *mcasdoor.CasdoorConnection, th *TransactionHandler, oh *OperationHandler, ah *AssetRateHandler) *fiber.App {
 	f := fiber.New(fiber.Config{
 		DisableStartupMessage: true,
 	})
@@ -42,6 +43,9 @@ func NewRouter(lg mlog.Logger, cc *mcasdoor.CasdoorConnection, th *TransactionHa
 	f.Get("/v1/organizations/:organization_id/ledgers/:ledger_id/portfolios/:portfolio_id/operations/:operation_id", jwt.ProtectHTTP(), jwt.WithPermissionHTTP("operations"), oh.GetOperationByPortfolio)
 
 	f.Patch("/v1/organizations/:organization_id/ledgers/:ledger_id/transactions/:transaction_id/operations/:operation_id", jwt.ProtectHTTP(), jwt.WithPermissionHTTP("operations"), lib.WithBody(new(o.UpdateOperationInput), oh.UpdateOperation))
+
+	// Asset-rate
+	f.Post("/v1/organizations/:organization_id/ledgers/:ledger_id/asset-rates", jwt.ProtectHTTP(), jwt.WithPermissionHTTP("asset-rate"), lib.ParseUUIDPathParameters, lib.WithBody(new(a.CreateAssetRateInput), ah.CreateAssetRate))
 
 	// Health
 	f.Get("/health", lib.Ping)
