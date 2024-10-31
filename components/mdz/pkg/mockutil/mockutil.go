697b036bdca1b750d5206e4e61b8af8a19b15c49
maxwelbm <maxwelbm@hotmail.com>
2024-10-31 16:50:55 -0300

    chore: G304 (CWE-22): Potential file inclusion via variable

--- components/mdz/pkg/mockutil/mockutil.go
+++ components/mdz/pkg/mockutil/mockutil.go
@@ -3,16 +3,18 @@ package mockutil
 import (
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/jarcoal/httpmock"
 )
 
 func loadJSONResponse(filename string) ([]byte, error) {
+	filename = filepath.Clean(filename)
 	return os.ReadFile(filename)
 }
 
-func MockResponseFromFile(status int, filepath string) httpmock.Responder {
-	data, err := loadJSONResponse(filepath)
+func MockResponseFromFile(status int, path string) httpmock.Responder {
+	data, err := loadJSONResponse(path)
 	if err != nil {
 		return httpmock.NewStringResponder(http.StatusInternalServerError,
 			"Failed to load mock response")
