10fdad41bbbff20f5efa88e015635128fc36bff2
maxwelbm <maxwelbm@hotmail.com>
2024-11-04 17:11:41 -0300

    refactor: rest organization use struct ledger

--- common/mmodel/organization.go
+++ common/mmodel/organization.go
@@ -1,27 +1,6 @@
-package organization
+package mmodel
 
-import (
-	"database/sql"
-	"time"
-
-	"github.com/LerianStudio/midaz/common"
-)
-
-// OrganizationPostgreSQLModel represents the entity Organization into SQL context in Database
-type OrganizationPostgreSQLModel struct {
-	ID                   string
-	ParentOrganizationID *string
-	LegalName            string
-	DoingBusinessAs      *string
-	LegalDocument        string
-	Address              Address
-	Status               string
-	StatusDescription    *string
-	CreatedAt            time.Time
-	UpdatedAt            time.Time
-	DeletedAt            sql.NullTime
-	Metadata             map[string]any
-}
+import "time"
 
 // CreateOrganizationInput is a struct design to encapsulate request create payload data.
 type CreateOrganizationInput struct {
@@ -59,15 +38,16 @@ type Organization struct {
 	Metadata             map[string]any `json:"metadata,omitempty"`
 }
 
+type Organizations struct {
+	Items []Organization `json:"items"`
+	Page  int            `json:"page"`
+	Limit int            `json:"limit"`
+}
+
 // Status structure for marshaling/unmarshalling JSON.
 type Status struct {
 	Code        string  `json:"code" validate:"max=100"`
 	Description *string `json:"description" validate:"omitempty,max=256"`
-}
-
-// IsEmpty method that set empty or nil in fields
-func (s Status) IsEmpty() bool {
-	return s.Code == "" && s.Description == nil
 }
 
 // Address structure for marshaling/unmarshalling JSON.
@@ -78,57 +58,4 @@ type Address struct {
 	City    string  `json:"city"`
 	State   string  `json:"state"`
 	Country string  `json:"country"` // According to ISO 3166-1 alpha-2
-}
-
-// IsEmpty method that set empty or nil in fields
-func (a Address) IsEmpty() bool {
-	return a.Line1 == "" && a.Line2 == nil && a.ZipCode == "" && a.City == "" && a.State == "" && a.Country == ""
-}
-
-// ToEntity converts an OrganizationPostgreSQLModel to entity.Organization
-func (t *OrganizationPostgreSQLModel) ToEntity() *Organization {
-	status := Status{
-		Code:        t.Status,
-		Description: t.StatusDescription,
-	}
-
-	organization := &Organization{
-		ID:                   t.ID,
-		ParentOrganizationID: t.ParentOrganizationID,
-		LegalName:            t.LegalName,
-		DoingBusinessAs:      t.DoingBusinessAs,
-		LegalDocument:        t.LegalDocument,
-		Address:              t.Address,
-		Status:               status,
-		CreatedAt:            t.CreatedAt,
-		UpdatedAt:            t.UpdatedAt,
-	}
-
-	if !t.DeletedAt.Time.IsZero() {
-		deletedAtCopy := t.DeletedAt.Time
-		organization.DeletedAt = &deletedAtCopy
-	}
-
-	return organization
-}
-
-// FromEntity converts an entity.Organization to OrganizationPostgresModel
-func (t *OrganizationPostgreSQLModel) FromEntity(organization *Organization) {
-	*t = OrganizationPostgreSQLModel{
-		ID:                   common.GenerateUUIDv7().String(),
-		ParentOrganizationID: organization.ParentOrganizationID,
-		LegalName:            organization.LegalName,
-		DoingBusinessAs:      organization.DoingBusinessAs,
-		LegalDocument:        organization.LegalDocument,
-		Address:              organization.Address,
-		Status:               organization.Status.Code,
-		StatusDescription:    organization.Status.Description,
-		CreatedAt:            organization.CreatedAt,
-		UpdatedAt:            organization.UpdatedAt,
-	}
-
-	if organization.DeletedAt != nil {
-		deletedAtCopy := *organization.DeletedAt
-		t.DeletedAt = sql.NullTime{Time: deletedAtCopy, Valid: true}
-	}
 }
