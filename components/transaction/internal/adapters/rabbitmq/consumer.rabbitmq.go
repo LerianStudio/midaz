4ff04d4295d7adf12f96815070c2f987bb6cc231
MartinezAvellan <martinez.avellan@hotmail.com>
2024-11-06 19:57:09 +0100

    feat: implement producer and consumer on adapters :sparkles:

---
+++ components/transaction/internal/adapters/rabbitmq/consumer.rabbitmq.go
@@ -0,0 +1,48 @@
+package mrabbitmq
+
+import (
+	"context"
+	"github.com/LerianStudio/midaz/common/mrabbitmq"
+)
+
+// ConsumerRabbitMQRepository is a rabbitmq implementation of the consumer
+type ConsumerRabbitMQRepository struct {
+	conn *mrabbitmq.RabbitMQConnection
+}
+
+// NewConsumerRabbitMQ returns a new instance of ConsumerRabbitMQRepository using the given rabbitmq connection.
+func NewConsumerRabbitMQ(c *mrabbitmq.RabbitMQConnection) *ConsumerRabbitMQRepository {
+	crmq := &ConsumerRabbitMQRepository{
+		conn: c,
+	}
+
+	_, err := c.GetNewConnect()
+	if err != nil {
+		panic("Failed to connect rabbitmq")
+	}
+
+	return crmq
+}
+
+func (crmq *ConsumerRabbitMQRepository) Consumer(ctx context.Context, queue string, response chan string) {
+
+	message, err := crmq.conn.Channel.Consume(
+		"transaction_operations_queue",
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		crmq.conn.Logger.Errorf("Failed to register a consumer: %s", err)
+	}
+
+	for d := range message {
+		crmq.conn.Logger.Infof("message consumed: %s", d.Body)
+
+		response <- string(d.Body[:])
+	}
+
+}
---
+++ components/transaction/internal/adapters/rabbitmq/producer.rabbitmq.go
@@ -0,0 +1,49 @@
+package mrabbitmq
+
+import (
+	"context"
+	"github.com/LerianStudio/midaz/common/mrabbitmq"
+	"github.com/streadway/amqp"
+)
+
+// ProducerRabbitMQRepository is a rabbitmq implementation of the producer
+type ProducerRabbitMQRepository struct {
+	conn *mrabbitmq.RabbitMQConnection
+}
+
+// NewProducerRabbitMQ returns a new instance of ProducerRabbitMQRepository using the given rabbitmq connection.
+func NewProducerRabbitMQ(c *mrabbitmq.RabbitMQConnection) *ProducerRabbitMQRepository {
+	prmq := &ProducerRabbitMQRepository{
+		conn: c,
+	}
+
+	_, err := c.GetNewConnect()
+	if err != nil {
+		panic("Failed to connect rabbitmq")
+	}
+
+	return prmq
+}
+
+func (prmq *ProducerRabbitMQRepository) Producer(ctx context.Context, exchange, key, body string) (*string, error) {
+	prmq.conn.Logger.Infoln("messages sent successfully")
+
+	err := prmq.conn.Channel.Publish(
+		"transaction_operations_exchange",
+		"transaction_operations_key",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Mensagem de conta contábil"),
+		})
+	if err != nil {
+		prmq.conn.Logger.Errorf("Failed to publish a message: %s", err)
+
+		return nil, err
+	}
+
+	prmq.conn.Logger.Infoln("messages sent successfully")
+
+	return nil, nil
+}
