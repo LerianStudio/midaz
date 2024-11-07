5dccc86eeb1847dc8dc99d835b6a0fed5888b043
MartinezAvellan <martinez.avellan@hotmail.com>
2024-11-07 16:39:10 +0100

    feat: implements producer and consumer with interface and implementation :sparkles:

---
+++ components/ledger/internal/adapters/rabbitmq/consumer.rabbitmq.go
@@ -0,0 +1,53 @@
+package mrabbitmq
+
+import (
+	"github.com/LerianStudio/midaz/common/mrabbitmq"
+)
+
+// ConsumerRabbitMQRepository is a rabbitmq implementation of the consumer
+type ConsumerRabbitMQRepository struct {
+	conn *mrabbitmq.RabbitMQConnection
+}
+
+// NewConsumerRabbitMQ returns a new instance of ConsumerRabbitMQRepository using the given rabbitmq connection.
+func NewConsumerRabbitMQ(c *mrabbitmq.RabbitMQConnection) *ConsumerRabbitMQRepository {
+	crmq := &ConsumerRabbitMQRepository{
+		conn: c,
+	}
+
+	_, err := c.GetNewConnect()
+	if err != nil {
+		panic("Failed to connect rabbitmq")
+	}
+
+	return crmq
+}
+
+func (crmq *ConsumerRabbitMQRepository) ConsumerDefault(response chan string) {
+	crmq.conn.Logger.Infoln("init consumer message")
+
+	message, err := crmq.conn.Channel.Consume(
+		crmq.conn.Queue,
+		"",
+		true,
+		false,
+		false,
+		false,
+		nil,
+	)
+	if err != nil {
+		crmq.conn.Logger.Errorf("Failed to register a consumer: %s", err)
+	}
+
+	forever := make(chan bool)
+
+	go func() {
+		for d := range message {
+			crmq.conn.Logger.Infof("message consumed: %s", d.Body)
+
+			//response <- string(d.Body[:])
+		}
+	}()
+
+	<-forever
+}
