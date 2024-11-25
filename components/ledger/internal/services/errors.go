c0d1d1419ef04ca4340a4f7071841cb587c54ea3
MartinezAvellan <martinez.avellan@hotmail.com>
2024-11-25 11:43:39 +0100

    fix: update imports :bug:

--- components/ledger/internal/services/errors.go
+++ components/ledger/internal/services/errors.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 
 	"github.com/LerianStudio/midaz/common"
-	cn "github.com/LerianStudio/midaz/common/constant"
+	"github.com/LerianStudio/midaz/common/constant"
 	"github.com/jackc/pgx/v5/pgconn"
 )
 
@@ -15,19 +15,19 @@ var ErrDatabaseItemNotFound = errors.New("errDatabaseItemNotFound")
 func ValidatePGError(pgErr *pgconn.PgError, entityType string) error {
 	switch pgErr.ConstraintName {
 	case "organization_parent_organization_id_fkey":
-		return common.ValidateBusinessError(cn.ErrParentOrganizationIDNotFound, entityType)
+		return common.ValidateBusinessError(constant.ErrParentOrganizationIDNotFound, entityType)
 	case "account_parent_account_id_fkey":
-		return common.ValidateBusinessError(cn.ErrInvalidParentAccountID, entityType)
+		return common.ValidateBusinessError(constant.ErrInvalidParentAccountID, entityType)
 	case "account_asset_code_fkey":
-		return common.ValidateBusinessError(cn.ErrAssetCodeNotFound, entityType)
+		return common.ValidateBusinessError(constant.ErrAssetCodeNotFound, entityType)
 	case "account_portfolio_id_fkey":
-		return common.ValidateBusinessError(cn.ErrPortfolioIDNotFound, entityType)
+		return common.ValidateBusinessError(constant.ErrPortfolioIDNotFound, entityType)
 	case "account_product_id_fkey":
-		return common.ValidateBusinessError(cn.ErrProductIDNotFound, entityType)
+		return common.ValidateBusinessError(constant.ErrProductIDNotFound, entityType)
 	case "account_ledger_id_fkey", "portfolio_ledger_id_fkey", "asset_ledger_id_fkey", "product_ledger_id_fkey":
-		return common.ValidateBusinessError(cn.ErrLedgerIDNotFound, entityType)
+		return common.ValidateBusinessError(constant.ErrLedgerIDNotFound, entityType)
 	case "account_organization_id_fkey", "ledger_organization_id_fkey", "asset_organization_id_fkey", "portfolio_organization_id_fkey", "product_organization_id_fkey":
-		return common.ValidateBusinessError(cn.ErrOrganizationIDNotFound, entityType)
+		return common.ValidateBusinessError(constant.ErrOrganizationIDNotFound, entityType)
 	default:
 		return pgErr
 	}
