28db38d0e391904458fd8234303f6f56b412e6c3
Clara Tersi <mclara.tersi@gmail.com>
2024-12-02 14:57:29 -0300

    feat(audit): ignore updatable fields for operation :sparkles:

--- components/audit/internal/adapters/rabbitmq/operation/operation.go
+++ components/audit/internal/adapters/rabbitmq/operation/operation.go
@@ -3,25 +3,21 @@ package operation
 import "time"
 
 type Operation struct {
-	ID              string         `json:"id" example:"00000000-0000-0000-0000-000000000000"`
-	TransactionID   string         `json:"transactionId" example:"00000000-0000-0000-0000-000000000000"`
-	Description     string         `json:"description" example:"Credit card operation"`
-	Type            string         `json:"type" example:"creditCard"`
-	AssetCode       string         `json:"assetCode" example:"BRL"`
-	ChartOfAccounts string         `json:"chartOfAccounts" example:"1000"`
-	Amount          Amount         `json:"amount"`
-	Balance         Balance        `json:"balance"`
-	BalanceAfter    Balance        `json:"balanceAfter"`
-	Status          Status         `json:"status"`
-	AccountID       string         `json:"accountId" example:"00000000-0000-0000-0000-000000000000"`
-	AccountAlias    string         `json:"accountAlias" example:"@person1"`
-	PortfolioID     *string        `json:"portfolioId" example:"00000000-0000-0000-0000-000000000000"`
-	OrganizationID  string         `json:"organizationId" example:"00000000-0000-0000-0000-000000000000"`
-	LedgerID        string         `json:"ledgerId" example:"00000000-0000-0000-0000-000000000000"`
-	CreatedAt       time.Time      `json:"createdAt" example:"2021-01-01T00:00:00Z"`
-	UpdatedAt       time.Time      `json:"updatedAt" example:"2021-01-01T00:00:00Z"`
-	DeletedAt       *time.Time     `json:"deletedAt" example:"2021-01-01T00:00:00Z"`
-	Metadata        map[string]any `json:"metadata"`
+	ID              string    `json:"id" example:"00000000-0000-0000-0000-000000000000"`
+	TransactionID   string    `json:"transactionId" example:"00000000-0000-0000-0000-000000000000"`
+	Type            string    `json:"type" example:"creditCard"`
+	AssetCode       string    `json:"assetCode" example:"BRL"`
+	ChartOfAccounts string    `json:"chartOfAccounts" example:"1000"`
+	Amount          Amount    `json:"amount"`
+	Balance         Balance   `json:"balance"`
+	BalanceAfter    Balance   `json:"balanceAfter"`
+	Status          Status    `json:"status"`
+	AccountID       string    `json:"accountId" example:"00000000-0000-0000-0000-000000000000"`
+	AccountAlias    string    `json:"accountAlias" example:"@person1"`
+	PortfolioID     *string   `json:"portfolioId" example:"00000000-0000-0000-0000-000000000000"`
+	OrganizationID  string    `json:"organizationId" example:"00000000-0000-0000-0000-000000000000"`
+	LedgerID        string    `json:"ledgerId" example:"00000000-0000-0000-0000-000000000000"`
+	CreatedAt       time.Time `json:"createdAt" example:"2021-01-01T00:00:00Z"`
 }
 
 type Amount struct {
