66d3b9379ac47475f9f32f9fe70e1c52ce9d46b7
Clara Tersi <mclara.tersi@gmail.com>
2024-12-02 15:02:03 -0300

    feat(audit): add get log by hash use case :sparkles:

---
+++ components/audit/internal/services/get-log-by-hash.go
@@ -0,0 +1,40 @@
+package services
+
+import (
+	"context"
+	"encoding/json"
+	"github.com/LerianStudio/midaz/components/audit/internal/adapters/rabbitmq/operation"
+	"github.com/LerianStudio/midaz/pkg"
+	"github.com/LerianStudio/midaz/pkg/mopentelemetry"
+)
+
+func (uc *UseCase) GetLogByHash(ctx context.Context, treeID int64, identityHash string) (operation.Operation, error) {
+	logger := pkg.NewLoggerFromContext(ctx)
+	tracer := pkg.NewTracerFromContext(ctx)
+
+	ctx, span := tracer.Start(ctx, "query.get_log_by_hash")
+	defer span.End()
+
+	log, err := uc.TrillianRepo.GetLogByHash(ctx, treeID, identityHash)
+	if err != nil {
+		mopentelemetry.HandleSpanError(&span, "Failed to get log by hash", err)
+
+		logger.Errorf("Error getting log by hash: %v", err)
+
+		return operation.Operation{}, err
+	}
+
+	var op operation.Operation
+
+	err = json.Unmarshal([]byte(log), &op)
+	if err != nil {
+		mopentelemetry.HandleSpanError(&span, "Failed to unmarshal log", err)
+
+		logger.Errorf("Error unmarshalling log: %v", err)
+
+		return operation.Operation{}, err
+	}
+
+	return op, nil
+
+}
