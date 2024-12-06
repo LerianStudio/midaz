3d8d8cd6ca41444132ef6682dede5e6f54859bc5
Clara Tersi <mclara.tersi@gmail.com>
2024-12-06 15:14:04 -0300

    fix(audit): update components/audit/internal/adapters/http/in/response.go
    Co-authored-by: Hugo Aguiar Martinez <martinez.avellan@hotmail.com>

--- components/audit/internal/adapters/http/in/response.go
+++ components/audit/internal/adapters/http/in/response.go
@@ -28,5 +28,5 @@ type HashValidationResponse struct {
 	AuditID        string `json:"auditId"`
 	ExpectedHash   string `json:"expectedHash"`
 	CalculatedHash string `json:"calculatedHash"`
-	WasTempered    bool   `json:"wasTempered"`
+	IsTampered    bool   `json:"isTampered"`
 } // @Name HashValidationResponse
