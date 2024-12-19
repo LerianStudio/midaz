961260fe86fe0995a848fe70879bf6aafed669c2
MartinezAvellan <martinez.avellan@hotmail.com>
2024-12-19 18:52:15 +0100

    refactor (transaction): swagger v3 :hammer:

--- components/transaction/internal/adapters/http/in/assetrate.go
+++ components/transaction/internal/adapters/http/in/assetrate.go
@@ -128,7 +128,7 @@ func (handler *AssetRateHandler) GetAssetRateByExternalID(c *fiber.Ctx) error {
 //	@Param			ledger_id		path		string		true	"Ledger ID"
 //	@Param			asset_code		path		string		true	"From Asset Code"
 //
-//	@Param			to				query		[]string	false	"To Asset Codes"	example("BRL,USD,SGD")
+//	@Param			to				query		[]string	false	"To Asset Codes"	example "BRL,USD,SGD"
 //	@Param			limit			query		int			false	"Limit"				default(10)
 //	@Param			start_date		query		string		false	"Start Date"		example "2021-01-01"
 //	@Param			end_date		query		string		false	"End Date"			example "2021-01-01"
