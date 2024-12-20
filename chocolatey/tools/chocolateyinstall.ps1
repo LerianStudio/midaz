fac503f593fe344ba47e3aebc9a4e880c5661d68
maxwelbm <maxwelbm@hotmail.com>
2024-12-20 13:39:43 -0300

    feat: publish cli midaz in the choco

---
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -0,0 +1,40 @@
+$ErrorActionPreference = 'Stop';
+
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$binDir     = Join-Path $env:ChocolateyInstall 'bin'
+$outputFile = Join-Path $toolsDir 'mdz.exe'
+
+# URL do arquivo zipado
+$url        = 'https://github.com/maxwelbm/mdz/releases/download/v0.5.7/mdz_0.5.7_windows_amd64.zip'
+$checksum   = '{{CHECKSUM}}'
+$silentArgs = ''
+
+# Argumentos do pacote
+$packageArgs = @{
+    packageName   = 'mdz'
+    unzipLocation = $toolsDir
+    url           = $url
+    softwareName  = 'mdz*'
+    checksum      = $checksum
+    checksumType  = 'sha256'
+}
+
+# Instalar e descompactar o pacote
+Install-ChocolateyZipPackage @packageArgs
+
+# Verificar se o arquivo .exe foi extraído corretamente
+if (-Not (Test-Path $outputFile)) {
+    throw "O arquivo mdz.exe não foi encontrado após a extração do zip."
+}
+
+# Certificar-se de que o diretório global 'bin' existe
+if (-Not (Test-Path $binDir)) {
+    New-Item -ItemType Directory -Path $binDir | Out-Null
+}
+
+# Mover o executável para o diretório global
+Write-Host "Copiando $outputFile para $binDir"
+Copy-Item -Path $outputFile -Destination $binDir -Force
+
+# Confirmar a instalação
+Write-Host "Instalação completa. O executável mdz está disponível globalmente."
