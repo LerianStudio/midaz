ad04b2bf79d735f0d1fd73c9e545b8e999f72d35
Leonardo Peixoto <leonardo.peixoto@fortics.com.br>
2025-01-22 15:35:00 -0300

    fix: add PortfolioID to account update functionality :sparkles: :sparkles: :bug: :bug:

--- pkg/mmodel/account.go
+++ pkg/mmodel/account.go
@@ -36,6 +36,7 @@ type UpdateAccountInput struct {
 	AllowReceiving *bool          `json:"allowReceiving" example:"true"`
 	Alias          *string        `json:"alias" validate:"omitempty,max=100,prohibitedexternalaccountprefix" example:"@person1"`
 	ProductID      *string        `json:"productId" validate:"omitempty,uuid" example:"00000000-0000-0000-0000-000000000000"`
+	PortfolioID    *string        `json:"portfolioId" validate:"omitempty,uuid" example:"00000000-0000-0000-0000-000000000000"`
 	Metadata       map[string]any `json:"metadata" validate:"dive,keys,keymax=100,endkeys,nonested,valuemax=2000"`
 } // @name UpdateAccountInput
 
