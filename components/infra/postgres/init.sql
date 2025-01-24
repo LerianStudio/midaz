79f3c14a30186fdb7d7d2bec5fd37af2e7b9e5a6
MartinezAvellan <martinez.avellan@hotmail.com>
2025-01-24 19:59:52 +0100

    refactor: change infra :hammer:

--- components/infra/postgres/init.sql
+++ components/infra/postgres/init.sql
@@ -1,5 +1,4 @@
 create user replicator with replication encrypted password 'replicator_password';
 select pg_create_physical_replication_slot('replication_slot');
 
-CREATE DATABASE ledger;
-CREATE DATABASE transaction;
+CREATE DATABASE "midaz-00000000-0000-0000-0000-000000000000";
