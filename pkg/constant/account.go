8ccb791e33af5377b945d7760f040041f285e63b
MartinezAvellan <martinez.avellan@hotmail.com>
2025-02-03 13:10:19 +0100

    fix; fine tunning time;

--- pkg/constant/account.go
+++ pkg/constant/account.go
@@ -6,9 +6,9 @@ const (
 	DefaultExternalAccountAliasPrefix       = "@external/"
 	ExternalAccountType                     = "external"
 	NumberOfLockRetry                 int64 = 3
-	TimeLockRetry                           = 20 * time.Millisecond
-	TimeSetLock                             = 10 * time.Millisecond
-	SetLockBalance                          = 15 * time.Millisecond
-	CheckAndReleaseLockBalance              = 16 * time.Millisecond
+	TimeLockRetry                           = 100 * time.Millisecond
+	TimeSetLock                             = 100 * time.Millisecond
+	SetLockBalance                          = 100 * time.Millisecond
+	CheckAndReleaseLockBalance              = 100 * time.Millisecond
 	TimeToSetAccountsInRedis                = 5 * time.Minute
 )
