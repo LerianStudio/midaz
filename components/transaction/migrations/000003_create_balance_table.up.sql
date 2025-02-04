8d06d22f70120932632fdb537f8b05977f71d564
MartinezAvellan <martinez.avellan@hotmail.com>
2025-02-04 19:07:01 +0100

    fix: remove extension; add index; :bug:

--- components/transaction/migrations/000003_create_balance_table.up.sql
+++ components/transaction/migrations/000003_create_balance_table.up.sql
@@ -1,5 +1,3 @@
-CREATE EXTENSION IF NOT EXISTS pgcrypto;
-
 CREATE TABLE IF NOT EXISTS balance (
   id                                  UUID PRIMARY KEY NOT NULL,
   alias                               TEXT NOT NULL,
@@ -16,23 +14,6 @@ CREATE TABLE IF NOT EXISTS balance (
   deleted_at                          TIMESTAMP WITH TIME ZONE
 );
 
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-1_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-2_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-3_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-4_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-5_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-6_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-7_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-8_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-9_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account1-10_BRL', gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-1_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-2_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-3_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-4_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-5_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-6_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-7_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-8_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-9_BRL',  gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
-INSERT INTO balance VALUES (gen_random_uuid(), '@account2-10_BRL', gen_random_uuid(), gen_random_uuid(),'BRL',0,0,0,0,TRUE,NOW(),NOW());
+CREATE INDEX idx_update_balance ON balance (id, alias, organization_id, ledger_id, version, deleted_at);
+
+REINDEX INDEX idx_update_balance;
