8791e042ef82aa03fa84b2015cf2077ac0685a25
MartinezAvellan <martinez.avellan@hotmail.com>
2025-02-28 12:08:08 +0100

    feat: create new index from database; remove dockerfile platform to predefined redundant;

--- components/onboarding/migrations/000004_create_portfolio_table.up.sql
+++ components/onboarding/migrations/000004_create_portfolio_table.up.sql
@@ -7,9 +7,12 @@ CREATE TABLE IF NOT EXISTS portfolio
     organization_id               UUID NOT NULL,
     status                        TEXT NOT NULL,
     status_description            TEXT,
-    created_at                    TIMESTAMP WITH TIME ZONE,
-    updated_at                    TIMESTAMP WITH TIME ZONE,
+    created_at                    TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at                    TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     deleted_at                    TIMESTAMP WITH TIME ZONE,
     FOREIGN KEY (ledger_id)       REFERENCES ledger (id),
     FOREIGN KEY (organization_id) REFERENCES organization (id)
 );
+
+CREATE INDEX idx_portfolio_created_at ON portfolio (created_at);
+REINDEX INDEX idx_portfolio_created_at;
--- components/transaction/migrations/000000_create_transaction_table.up.sql
+++ components/transaction/migrations/000000_create_transaction_table.up.sql
@@ -12,8 +12,14 @@ CREATE TABLE IF NOT EXISTS "transaction" (
     ledger_id                           UUID NOT NULL,
     organization_id                     UUID NOT NULL,
     body                                JSONB NOT NULL,
-    created_at                          TIMESTAMP WITH TIME ZONE,
-    updated_at                          TIMESTAMP WITH TIME ZONE,
+    created_at                          TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at                          TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
     deleted_at                          TIMESTAMP WITH TIME ZONE,
     FOREIGN KEY (parent_transaction_id) REFERENCES "transaction" (id)
-)
+);
+
+CREATE INDEX idx_transaction_organization_ledger_id ON "transaction" (organization_id, ledger_id);
+REINDEX INDEX idx_transaction_organization_ledger_id;
+
+CREATE INDEX idx_transaction_created_at ON "transaction" (created_at);
+REINDEX INDEX idx_transaction_created_at;
