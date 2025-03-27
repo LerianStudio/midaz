8104d3debce27b4a6cd675113da0dac134b333ef
MartinezAvellan <martinez.avellan@hotmail.com>
2025-03-27 20:31:32 +0100

    feat: adjust mongo.sh to init configs; :sparkles:

--- components/infra/mongo/mongo.sh
+++ components/infra/mongo/mongo.sh
@@ -10,16 +10,11 @@ done
 
 echo "initializing replica set... 🚀 "
 
-IS_REPLICA_INITIALIZED=$(mongosh --quiet --host "${MONGO_HOST}:${MONGO_PORT}" -u "${MONGO_USER}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin --eval "rs.status().ok")
-
-if [ "$IS_REPLICA_INITIALIZED" = "1" ]; then
-  echo "replica set already initialized, skipping. ✅ "
-else
-  mongosh --host "${MONGO_HOST}:${MONGO_PORT}" -u "${MONGO_USER}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin <<EOF
-      rs.initiate({
-        _id: "rs0",
-        members: [{ _id: 0, host: "${MONGO_HOST}:${MONGO_PORT}" }]
-      });
+mongosh --host "${MONGO_HOST}:${MONGO_PORT}" -u "${MONGO_USER}" -p "${MONGO_PASSWORD}" --authenticationDatabase admin <<EOF
+    rs.initiate({
+      _id: "rs0",
+      members: [{ _id: 0, host: "${MONGO_HOST}:${MONGO_PORT}" }]
+    });
 EOF
-  echo "mongodb replica set initialization complete! ✅ "
-fi
+
+echo "mongodb replica set initialization complete! ✅ "
