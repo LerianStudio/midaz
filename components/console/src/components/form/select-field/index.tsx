e49f555e363b1e245ad30f9cf60acaba61980774
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-05-06 11:35:32 -0300

    feat: Implemented Multi on SelectField

--- components/console/src/components/form/select-field/index.tsx
+++ components/console/src/components/form/select-field/index.tsx
@@ -8,12 +8,19 @@ import {
   FormTooltip
 } from '@/components/ui/form'
 import {
+  MultipleSelect,
+  MultipleSelectContent,
+  MultipleSelectTrigger,
+  MultipleSelectValue
+} from '@/components/ui/multiple-select'
+import {
   Select,
   SelectContent,
   SelectTrigger,
   SelectValue
 } from '@/components/ui/select'
 import { cn } from '@/lib/utils'
+import { CommandGroup } from 'cmdk'
 import { PropsWithChildren, ReactNode } from 'react'
 import { Control } from 'react-hook-form'
 
@@ -26,6 +33,7 @@ export type SelectFieldProps = PropsWithChildren & {
   placeholder?: string
   disabled?: boolean
   control: Control<any>
+  multi?: boolean
   required?: boolean
 }
 
@@ -37,6 +45,7 @@ export const SelectField = ({
   placeholder,
   description,
   disabled,
+  multi,
   children,
   ...others
 }: SelectFieldProps) => {
@@ -54,17 +63,31 @@ export const SelectField = ({
               {label}
             </FormLabel>
           )}
-          <Select onValueChange={onChange} {...fieldOthers}>
-            <FormControl>
-              <SelectTrigger
-                disabled={disabled}
-                className={cn(disabled && 'bg-shadcn-100')}
-              >
-                <SelectValue placeholder={placeholder} />
-              </SelectTrigger>
-            </FormControl>
-            <SelectContent>{children}</SelectContent>
-          </Select>
+          {multi && (
+            <MultipleSelect
+              onValueChange={onChange}
+              disabled={disabled}
+              {...fieldOthers}
+            >
+              <MultipleSelectTrigger>
+                <MultipleSelectValue placeholder={placeholder} />
+              </MultipleSelectTrigger>
+              <MultipleSelectContent>{children}</MultipleSelectContent>
+            </MultipleSelect>
+          )}
+          {!multi && (
+            <Select onValueChange={onChange} {...fieldOthers}>
+              <FormControl>
+                <SelectTrigger
+                  disabled={disabled}
+                  className={cn(disabled && 'bg-shadcn-100')}
+                >
+                  <SelectValue placeholder={placeholder} />
+                </SelectTrigger>
+              </FormControl>
+              <SelectContent>{children}</SelectContent>
+            </Select>
+          )}
           <FormMessage />
           {description && <FormDescription>{description}</FormDescription>}
         </FormItem>
