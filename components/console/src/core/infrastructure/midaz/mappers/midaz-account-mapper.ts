fb55472da77f9ddfba5e82f9ea4657c3adb7dba5
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-05-20 13:45:37 -0300

    fix: Fields missing on Accounts mapper

--- components/console/src/core/infrastructure/midaz/mappers/midaz-account-mapper.ts
+++ components/console/src/core/infrastructure/midaz/mappers/midaz-account-mapper.ts
@@ -39,19 +39,22 @@ export class MidazAccountMapper {
     }
   }
 
-  public static toEntity(asset: MidazAccountDto): AccountEntity {
+  public static toEntity(account: MidazAccountDto): AccountEntity {
     return {
-      id: asset.id,
-      organizationId: asset.organizationId,
-      ledgerId: asset.ledgerId,
-      name: asset.name,
-      type: asset.type,
-      alias: asset.alias,
-      assetCode: asset.assetCode,
-      metadata: asset.metadata ?? {},
-      createdAt: asset.createdAt,
-      updatedAt: asset.updatedAt,
-      deletedAt: asset.deletedAt
+      id: account.id,
+      organizationId: account.organizationId,
+      ledgerId: account.ledgerId,
+      name: account.name,
+      type: account.type,
+      alias: account.alias,
+      assetCode: account.assetCode,
+      entityId: account.entityId!,
+      portfolioId: account.portfolioId!,
+      segmentId: account.segmentId!,
+      metadata: account.metadata ?? {},
+      createdAt: account.createdAt,
+      updatedAt: account.updatedAt,
+      deletedAt: account.deletedAt
     }
   }
 
