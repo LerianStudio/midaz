86f8429ca5f35f8b9064ac99b3abe244910778c8
Caio Alexandre Troti Caetano <caio_aletroca@hotmail.com>
2025-05-30 15:35:30 -0300

    fix: Removed legacy eslintrc config

--- components/console/eslint.config.mjs
+++ components/console/eslint.config.mjs
@@ -1,31 +1,34 @@
-import { defineConfig } from "eslint/config";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import { defineConfig } from 'eslint/config'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all
+})
 
-export default defineConfig([{
+export default defineConfig([
+  {
     extends: compat.extends(
-        "next",
-        "next/core-web-vitals",
-        "plugin:prettier/recommended",
-        "plugin:storybook/recommended",
+      'next',
+      'next/core-web-vitals',
+      'plugin:prettier/recommended',
+      'plugin:storybook/recommended'
     ),
 
     rules: {
-        "prettier/prettier": "error",
-        "react-hooks/rules-of-hooks": "off",
-        "react-hooks/exhaustive-deps": "off",
-    },
-}, {
-    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
-    rules: {},
-}]);
+      'prettier/prettier': 'error',
+      'react-hooks/rules-of-hooks': 'off',
+      'react-hooks/exhaustive-deps': 'off'
+    }
+  },
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    rules: {}
+  }
+])
