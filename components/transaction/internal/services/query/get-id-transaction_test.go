a898aabe0b9f9034b8c464e3b8cc7e32636eeec8
MartinezAvellan <martinez.avellan@hotmail.com>
2025-06-18 13:28:40 +0100

    fix: add tests;

--- components/transaction/internal/services/query/get-id-transaction_test.go
+++ components/transaction/internal/services/query/get-id-transaction_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"errors"
 	libCommons "github.com/LerianStudio/lib-commons/commons"
+	"github.com/LerianStudio/midaz/components/transaction/internal/adapters/postgres/operation"
 	"github.com/LerianStudio/midaz/components/transaction/internal/adapters/postgres/transaction"
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/mock/gomock"
@@ -52,6 +53,55 @@ func TestGetTransactionByIDError(t *testing.T) {
 		Return(nil, errors.New(errMSG)).
 		Times(1)
 	res, err := uc.TransactionRepo.Find(context.TODO(), organizationID, ledgerID, ID)
+
+	assert.NotEmpty(t, err)
+	assert.Equal(t, err.Error(), errMSG)
+	assert.Nil(t, res)
+}
+
+func TestGetTransactionWithOperationsByID(t *testing.T) {
+	ID := libCommons.GenerateUUIDv7()
+	organizationID := libCommons.GenerateUUIDv7()
+	ledgerID := libCommons.GenerateUUIDv7()
+
+	tran := &transaction.Transaction{
+		ID:             ID.String(),
+		OrganizationID: organizationID.String(),
+		LedgerID:       ledgerID.String(),
+		Operations:     make([]*operation.Operation, 1),
+	}
+
+	uc := UseCase{
+		TransactionRepo: transaction.NewMockRepository(gomock.NewController(t)),
+	}
+
+	uc.TransactionRepo.(*transaction.MockRepository).
+		EXPECT().
+		FindWithOperations(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(tran, nil).
+		Times(1)
+	res, err := uc.TransactionRepo.FindWithOperations(context.TODO(), organizationID, ledgerID, ID)
+
+	assert.Equal(t, tran, res)
+	assert.Nil(t, err)
+}
+
+func TestGetTransactionWithOperationsByIDError(t *testing.T) {
+	errMSG := "err to get transaction with operations from database"
+	ID := libCommons.GenerateUUIDv7()
+	organizationID := libCommons.GenerateUUIDv7()
+	ledgerID := libCommons.GenerateUUIDv7()
+
+	uc := UseCase{
+		TransactionRepo: transaction.NewMockRepository(gomock.NewController(t)),
+	}
+
+	uc.TransactionRepo.(*transaction.MockRepository).
+		EXPECT().
+		FindWithOperations(gomock.Any(), gomock.Any(), gomock.Any(), gomock.Any()).
+		Return(nil, errors.New(errMSG)).
+		Times(1)
+	res, err := uc.TransactionRepo.FindWithOperations(context.TODO(), organizationID, ledgerID, ID)
 
 	assert.NotEmpty(t, err)
 	assert.Equal(t, err.Error(), errMSG)
