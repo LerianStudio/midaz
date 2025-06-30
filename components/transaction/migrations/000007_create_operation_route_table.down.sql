7c8341fa8061616b7a1202832162fcdc3d78defc
Clara Tersi <mclara.tersi@gmail.com>
2025-06-30 17:14:31 -0300

    chore(migrations): remove indexes and drop tables for operation and transaction routes

--- components/transaction/migrations/000007_create_operation_route_table.down.sql
+++ components/transaction/migrations/000007_create_operation_route_table.down.sql
@@ -1,1 +1,5 @@
+DROP INDEX IF EXISTS idx_operation_route_deleted_at;
+DROP INDEX IF EXISTS idx_operation_route_type;
+DROP INDEX IF EXISTS idx_operation_route_organization_id_ledger_id;
+
 DROP TABLE IF EXISTS operation_route;
