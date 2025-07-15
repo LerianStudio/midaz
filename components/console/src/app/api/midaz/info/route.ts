4017126cbaed4a21f565fd3554996eb8ede9e80d
Augusto Alvarenga <augusto.santesso@gmail.com>
2025-07-15 11:53:56 -0300

    refactor: enhance MidazInfo API with middleware
    - Integrated logger middleware to the GET endpoint for improved logging of fetch operations.
    - Maintained the structure of the MidazInfoDto response while ensuring middleware functionality.

--- components/console/src/app/api/midaz/info/route.ts
+++ components/console/src/app/api/midaz/info/route.ts
@@ -1,10 +1,19 @@
 import { MidazInfoDto } from '@/core/application/dto/midaz-info-dto'
+import { applyMiddleware } from '@/lib/middleware'
+import { loggerMiddleware } from '@/utils/logger-middleware-config'
 import { NextResponse } from 'next/server'
 
-export async function GET() {
-  const midazInfoDto: MidazInfoDto = {
-    version: process.env.VERSION || '1.0.0'
+export const GET = applyMiddleware(
+  [
+    loggerMiddleware({
+      operationName: 'fetchMidazInfo',
+      method: 'GET'
+    })
+  ],
+  async () => {
+    const midazInfoDto: MidazInfoDto = {
+      version: process.env.VERSION || '1.0.0'
+    }
+    return NextResponse.json(midazInfoDto)
   }
-
-  return NextResponse.json(midazInfoDto)
-}
+)
