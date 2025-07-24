f9df300f6bee0b36fe71fec320ddf062ab13e4f6
Augusto Alvarenga <augusto.santesso@gmail.com>
2025-07-24 11:48:40 -0300

    fix: docker compose config

--- components/console/next.config.mjs
+++ components/console/next.config.mjs
@@ -6,10 +6,6 @@ const nextConfig = {
       fullUrl: true
     }
   },
-  env: {
-    MIDAZ_CONSOLE_BASE_PATH: process.env.MIDAZ_CONSOLE_BASE_PATH,
-    MIDAZ_SERVER_BASE_PATH: process.env.MIDAZ_SERVER_BASE_PATH
-  },
   headers: async () => {
     return [
       {
