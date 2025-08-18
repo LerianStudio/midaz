f9e860590303128eaf4f262eacecc981e7ad745e
Brecci <brecci@lerian.studio>
2025-08-18 16:34:52 -0300

    fix: correct indentation in asset queries and update UUID path parameter parsing :bug:

--- components/onboarding/internal/adapters/postgres/asset/asset.postgresql.go
+++ components/onboarding/internal/adapters/postgres/asset/asset.postgresql.go
@@ -161,7 +161,6 @@ func (r *AssetPostgreSQLRepository) FindByNameOrCode(ctx context.Context, organi
 	spanQuery.End()
 
 	if rows.Next() {
-
 		logger.Warnf("Asset name or code duplicate found: %v", err)
 		err := pkg.ValidateBusinessError(constant.ErrAssetNameOrCodeDuplicate, reflect.TypeOf(mmodel.Asset{}).Name())
 
@@ -232,7 +231,7 @@ func (r *AssetPostgreSQLRepository) FindAll(ctx context.Context, organizationID,
 			&asset.LedgerID, &asset.OrganizationID, &asset.CreatedAt, &asset.UpdatedAt, &asset.DeletedAt); err != nil {
 			libOpentelemetry.HandleSpanError(&span, "Failed to scan row", err)
 
-		logger.Errorf("Failed to scan row: %v", err)
+			logger.Errorf("Failed to scan row: %v", err)
 
 			return nil, err
 		}
@@ -290,7 +289,7 @@ func (r *AssetPostgreSQLRepository) ListByIDs(ctx context.Context, organizationI
 			&asset.LedgerID, &asset.OrganizationID, &asset.CreatedAt, &asset.UpdatedAt, &asset.DeletedAt); err != nil {
 			libOpentelemetry.HandleSpanError(&span, "Failed to scan row", err)
 
-		logger.Errorf("Failed to scan row: %v", err)
+			logger.Errorf("Failed to scan row: %v", err)
 
 			return nil, err
 		}
