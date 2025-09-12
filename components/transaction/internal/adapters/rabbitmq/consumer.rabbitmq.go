18448b00a4789be55d96407e18dbb319ab8665e3
Clara Tersi <mclara.tersi@gmail.com>
2025-09-12 17:16:04 -0300

    fix(rabbitmq): consumer reconnection

--- components/transaction/internal/adapters/rabbitmq/consumer.rabbitmq.go
+++ components/transaction/internal/adapters/rabbitmq/consumer.rabbitmq.go
@@ -2,12 +2,15 @@ package rabbitmq
 
 import (
 	"context"
+	"time"
 
 	libCommons "github.com/LerianStudio/lib-commons/v2/commons"
 	libConstants "github.com/LerianStudio/lib-commons/v2/commons/constants"
 	libLog "github.com/LerianStudio/lib-commons/v2/commons/log"
 	libOpentelemetry "github.com/LerianStudio/lib-commons/v2/commons/opentelemetry"
 	libRabbitmq "github.com/LerianStudio/lib-commons/v2/commons/rabbitmq"
+	"github.com/LerianStudio/midaz/v3/pkg/utils"
+	amqp "github.com/rabbitmq/amqp091-go"
 	attribute "go.opentelemetry.io/otel/attribute"
 )
 
@@ -63,83 +66,134 @@ func (cr *ConsumerRoutes) Register(queueName string, handler QueueHandlerFunc) {
 	cr.routes[queueName] = handler
 }
 
-// RunConsumers  init consume for all registry queues.
+// RunConsumers init consume for all registry queues.
 func (cr *ConsumerRoutes) RunConsumers() error {
 	for queueName, handler := range cr.routes {
 		cr.Infof("Initializing consumer for queue: %s", queueName)
 
-		err := cr.conn.Channel.Qos(
-			cr.NumbersOfPrefetch,
-			0,
-			false,
-		)
-		if err != nil {
-			return err
-		}
+		go func(queueName string, handler QueueHandlerFunc) {
+			backoff := utils.InitialBackoff
 
-		messages, err := cr.conn.Channel.Consume(
-			queueName,
-			"",
-			false,
-			false,
-			false,
-			false,
-			nil,
-		)
-		if err != nil {
-			return err
-		}
+			for {
+				if err := cr.conn.EnsureChannel(); err != nil {
+					cr.Errorf("[Consumer %s] failed to ensure channel: %v", queueName, err)
 
-		for i := 0; i < cr.NumbersOfWorkers; i++ {
-			go func(workerID int, queue string, handlerFunc QueueHandlerFunc) {
-				for msg := range messages {
-					midazID, found := msg.Headers[libConstants.HeaderID]
-					if !found {
-						midazID = libCommons.GenerateUUIDv7().String()
-					}
+					sleepDuration := utils.FullJitter(backoff)
+					cr.Infof("[Consumer %s] retrying EnsureChannel in %v...", queueName, sleepDuration)
+					time.Sleep(sleepDuration)
 
-					log := cr.Logger.WithFields(
-						libConstants.HeaderID, midazID.(string),
-					).WithDefaultMessageTemplate(midazID.(string) + libConstants.LoggerDefaultSeparator)
+					backoff = utils.NextBackoff(backoff)
 
-					ctx := libCommons.ContextWithLogger(
-						libCommons.ContextWithHeaderID(context.Background(), midazID.(string)),
-						log,
-					)
-
-					ctx = libCommons.ContextWithHeaderID(ctx, midazID.(string))
-					ctx = libOpentelemetry.ExtractTraceContextFromQueueHeaders(ctx, msg.Headers)
-
-					logger, tracer, reqId, _ := libCommons.NewTrackingFromContext(ctx)
-					ctx, spanConsumer := tracer.Start(ctx, "rabbitmq.consumer.process_message")
-
-					ctx = libCommons.ContextWithSpanAttributes(ctx, attribute.String("app.request.request_id", reqId))
-
-					err := libOpentelemetry.SetSpanAttributesFromStruct(&spanConsumer, "app.request.rabbitmq.consumer.message", msg.Body)
-					if err != nil {
-						libOpentelemetry.HandleSpanError(&spanConsumer, "Failed to convert message to JSON string", err)
-					}
-
-					err = handlerFunc(ctx, msg.Body)
-					if err != nil {
-						libOpentelemetry.HandleSpanBusinessErrorEvent(&spanConsumer, "Error processing message from queue", err)
-
-						spanConsumer.End()
-
-						logger.Errorf("Worker %d: Error processing message from queue %s: %v", workerID, queue, err)
-
-						_ = msg.Nack(false, true)
-
-						continue
-					}
-
-					spanConsumer.End()
-
-					_ = msg.Ack(false)
+					continue
 				}
-			}(i, queueName, handler)
-		}
+
+				if err := cr.conn.Channel.Qos(
+					cr.NumbersOfPrefetch,
+					0,
+					false,
+				); err != nil {
+					cr.Errorf("[Consumer %s] failed to set QoS: %v", queueName, err)
+
+					sleepDuration := utils.FullJitter(backoff)
+					cr.Infof("[Consumer %s] retrying QoS in %v...", queueName, sleepDuration)
+					time.Sleep(sleepDuration)
+
+					backoff = utils.NextBackoff(backoff)
+
+					continue
+				}
+
+				messages, err := cr.conn.Channel.Consume(
+					queueName,
+					"",
+					false,
+					false,
+					false,
+					false,
+					nil,
+				)
+				if err != nil {
+					cr.Errorf("[Consumer %s] failed to start consuming: %v", queueName, err)
+
+					sleepDuration := utils.FullJitter(backoff)
+					cr.Infof("[Consumer %s] retrying Consume in %v...", queueName, sleepDuration)
+					time.Sleep(sleepDuration)
+
+					backoff = utils.NextBackoff(backoff)
+
+					continue
+				}
+
+				cr.Infof("[Consumer %s] consuming started", queueName)
+
+				backoff = utils.InitialBackoff
+
+				notifyClose := make(chan *amqp.Error, 1)
+				cr.conn.Channel.NotifyClose(notifyClose)
+
+				for i := 0; i < cr.NumbersOfWorkers; i++ {
+					go cr.startWorker(i, queueName, handler, messages)
+				}
+
+				if errClose := <-notifyClose; errClose != nil {
+					cr.Warnf("[Consumer %s] channel closed: %v", queueName, errClose)
+				} else {
+					cr.Warnf("[Consumer %s] channel closed: no error info", queueName)
+				}
+
+				cr.Warnf("[Consumer %s] restarting...", queueName)
+			}
+		}(queueName, handler)
 	}
 
 	return nil
+}
+
+// startWorker inicia um worker que processa mensagens da fila.
+func (cr *ConsumerRoutes) startWorker(workerID int, queue string, handlerFunc QueueHandlerFunc, messages <-chan amqp.Delivery) {
+	for msg := range messages {
+		midazID, found := msg.Headers[libConstants.HeaderID]
+		if !found {
+			midazID = libCommons.GenerateUUIDv7().String()
+		}
+
+		log := cr.Logger.WithFields(
+			libConstants.HeaderID, midazID.(string),
+		).WithDefaultMessageTemplate(midazID.(string) + libConstants.LoggerDefaultSeparator)
+
+		ctx := libCommons.ContextWithLogger(
+			libCommons.ContextWithHeaderID(context.Background(), midazID.(string)),
+			log,
+		)
+
+		ctx = libCommons.ContextWithHeaderID(ctx, midazID.(string))
+		ctx = libOpentelemetry.ExtractTraceContextFromQueueHeaders(ctx, msg.Headers)
+
+		logger, tracer, reqId, _ := libCommons.NewTrackingFromContext(ctx)
+		ctx, spanConsumer := tracer.Start(ctx, "rabbitmq.consumer.process_message")
+
+		ctx = libCommons.ContextWithSpanAttributes(ctx, attribute.String("app.request.request_id", reqId))
+
+		err := libOpentelemetry.SetSpanAttributesFromStruct(&spanConsumer, "app.request.rabbitmq.consumer.message", msg.Body)
+		if err != nil {
+			libOpentelemetry.HandleSpanError(&spanConsumer, "Failed to convert message to JSON string", err)
+		}
+
+		err = handlerFunc(ctx, msg.Body)
+		if err != nil {
+			libOpentelemetry.HandleSpanBusinessErrorEvent(&spanConsumer, "Error processing message from queue", err)
+			spanConsumer.End()
+			logger.Errorf("Worker %d: Error processing message from queue %s: %v", workerID, queue, err)
+
+			_ = msg.Nack(false, true)
+
+			continue
+		}
+
+		spanConsumer.End()
+
+		_ = msg.Ack(false)
+	}
+
+	cr.Warnf("[Consumer %s] worker %d stopped (channel closed)", queue, workerID)
 }
