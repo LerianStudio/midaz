8601b62aeab28547937db99f43c5cfce1f298017
Fred Amaral <fred@fredamaral.com.br>
2025-09-15 13:47:23 -0300

    fix(tests): replace deprecated rand.Seed with local random generator
    - Remove deprecated rand.Seed usage (deprecated since Go 1.20)
    - Use rand.New(rand.NewSource()) for local random generator
    - Update RandString to use the local generator instance

--- tests/helpers/random.go
+++ tests/helpers/random.go
@@ -4,17 +4,17 @@ import (
     crand "crypto/rand"
     "encoding/hex"
     "math/rand"
-    "time"
 )
 
-// Seed math/rand once per test run.
-func init() { rand.Seed(time.Now().UnixNano()) }
+// globalRand provides a properly seeded random source for test data generation.
+// As of Go 1.20, the global rand functions are automatically seeded.
+var globalRand = rand.New(rand.NewSource(rand.Int63()))
 
 func RandString(n int) string {
     letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
     b := make([]rune, n)
     for i := range b {
-        b[i] = letters[rand.Intn(len(letters))]
+        b[i] = letters[globalRand.Intn(len(letters))]
     }
     return string(b)
 }
