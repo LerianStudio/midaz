e3ba4cbf79d0161c711b9b688ad03aa4b31325fd
Vagnerd <vagner.rodrigues@gmail.com>
2025-09-19 20:14:57 -0300

    Fix MongoDBParameters env name

--- components/transaction/internal/bootstrap/config.go
+++ components/transaction/internal/bootstrap/config.go
@@ -55,7 +55,7 @@ type Config struct {
 	MongoDBUser                  string `env:"MONGO_USER"`
 	MongoDBPassword              string `env:"MONGO_PASSWORD"`
 	MongoDBPort                  string `env:"MONGO_PORT"`
-	MongoDBParamters             string `env:"MONGO_PARAMETERS"`
+	MongoDBParameters             string `env:"MONGO_PARAMETERS"`
 	MaxPoolSize                  int    `env:"MONGO_MAX_POOL_SIZE"`
 	CasdoorAddress               string `env:"CASDOOR_ADDRESS"`
 	CasdoorClientID              string `env:"CASDOOR_CLIENT_ID"`
