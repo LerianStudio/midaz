e49036b34a27586bac7f32d1d593e5d13e6ed071
Augusto Alvarenga <augusto.santesso@gmail.com>
2025-09-23 15:26:30 -0300

    chore: add missing newline at end of JSON files and clean up middleware.ts

--- components/console/src/middleware.ts
+++ components/console/src/middleware.ts
@@ -3,12 +3,8 @@ import { applyMiddleware } from './lib/masterMiddleware'
 import apiCorsMiddleware from './middlewares/corsMiddleware'
 
 export async function middleware(request: NextRequest, event: NextFetchEvent) {
-  
-
   return applyMiddleware([], () => NextResponse.next(), [apiCorsMiddleware()])(
     request,
     event
   )
 }
-
-
