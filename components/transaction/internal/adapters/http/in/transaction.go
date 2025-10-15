4a58a0ac44dd729fecadb939ece3fa7b73b7710c
Clara Tersi <mclara.tersi@gmail.com>
2025-10-15 16:56:29 -0300

    feat(transaction): include version field in balance structure

--- components/transaction/internal/adapters/http/in/transaction.go
+++ components/transaction/internal/adapters/http/in/transaction.go
@@ -776,11 +776,13 @@ func (handler *TransactionHandler) BuildOperations(
 				balance := operation.Balance{
 					Available: &blc.Available,
 					OnHold:    &blc.OnHold,
+					Version:   &blc.Version,
 				}
 
 				balanceAfter := operation.Balance{
 					Available: &bat.Available,
 					OnHold:    &bat.OnHold,
+					Version:   &bat.Version,
 				}
 
 				if isAnnotation {
@@ -791,6 +793,10 @@ func (handler *TransactionHandler) BuildOperations(
 					o := decimal.NewFromInt(0)
 					balance.OnHold = &o
 					balanceAfter.OnHold = &o
+
+					v := int64(0)
+					balance.Version = &v
+					balanceAfter.Version = &v
 				}
 
 				description := fromTo[i].Description
