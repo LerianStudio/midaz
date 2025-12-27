0ebc59513189ca953e07a3bcac5f67a98e52fdb0
Fred Amaral <fred@fredamaral.com.br>
2025-12-26 23:22:07 -0300

    test(transaction): add missing imports for create-operation tests
    - Add pkgTransaction import for test utilities
    - Add require import for assertion helpers

--- components/transaction/internal/services/command/create-operation_test.go
+++ components/transaction/internal/services/command/create-operation_test.go
@@ -7,7 +7,9 @@ import (
 
 	libCommons "github.com/LerianStudio/lib-commons/v2/commons"
 	"github.com/LerianStudio/midaz/v3/components/transaction/internal/adapters/postgres/operation"
+	pkgTransaction "github.com/LerianStudio/midaz/v3/pkg/transaction"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 )
 
