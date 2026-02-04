fc20b993902d8e73439da630655207bf46eeb4e1
Ygohr Medeiros <medeirosygohr@gmail.com>
2026-02-04 15:14:15 -0300

    refactor: remove race condition :hammer:

--- components/transaction/internal/adapters/rabbitmq/healthcheck.go
+++ components/transaction/internal/adapters/rabbitmq/healthcheck.go
@@ -272,7 +272,11 @@ func (s *StateAwareHealthChecker) startRecoveryMonitor() {
 }
 
 // stopRecoveryMonitor stops the recovery monitor goroutine.
+// It acquires startStopMu to safely access s.stopMonitor.
 func (s *StateAwareHealthChecker) stopRecoveryMonitor() {
+	s.startStopMu.Lock()
+	defer s.startStopMu.Unlock()
+
 	if s.stopMonitor != nil {
 		close(s.stopMonitor)
 		s.stopMonitor = nil
