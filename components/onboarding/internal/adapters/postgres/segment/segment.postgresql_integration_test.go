417605b6635d13b676c56ce3627108d79b905f57
Clara Tersi <mclara.tersi@gmail.com>
2026-02-05 13:02:34 -0300

    fix: move build constraints before license headers in integration tests :bug:

--- components/onboarding/internal/adapters/postgres/segment/segment.postgresql_integration_test.go
+++ components/onboarding/internal/adapters/postgres/segment/segment.postgresql_integration_test.go
@@ -1,8 +1,8 @@
+//go:build integration
+
 // Copyright (c) 2026 Lerian Studio. All rights reserved.
 // Use of this source code is governed by the Elastic License 2.0
 // that can be found in the LICENSE file.
-
-//go:build integration
 
 package segment
 
