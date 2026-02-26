1fce44d9380d2e1283b4ebbd675440c26091a1f6
Ygohr Medeiros <medeirosygohr@gmail.com>
2026-02-26 12:20:46 -0300

    refactor: lint issue :hammer:

--- pkg/mbootstrap/settings.go
+++ pkg/mbootstrap/settings.go
@@ -32,4 +32,3 @@ type SettingsPort interface {
 	// Returns an error if the ledger does not exist.
 	GetLedgerSettings(ctx context.Context, organizationID, ledgerID uuid.UUID) (map[string]any, error)
 }
-
