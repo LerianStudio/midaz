63a5c6b09e6e1d879b2adb2ed9d4582a42aab984
Clara Tersi <mclara.tersi@gmail.com>
2026-03-12 14:21:05 -0300

    feat: add cache penetration defense with not-found sentinel :sparkles:

--- components/transaction/internal/services/query/get-or-create-transaction-route-cache.go
+++ components/transaction/internal/services/query/get-or-create-transaction-route-cache.go
@@ -5,12 +5,16 @@
 package query
 
 import (
+	"bytes"
 	"context"
+	"errors"
 	"fmt"
+	"time"
 
 	libCommons "github.com/LerianStudio/lib-commons/v4/commons"
 	libOpentelemetry "github.com/LerianStudio/lib-commons/v4/commons/opentelemetry"
 	"github.com/LerianStudio/midaz/v3/components/transaction/internal/services"
+	pkg "github.com/LerianStudio/midaz/v3/pkg"
 	"github.com/LerianStudio/midaz/v3/pkg/mmodel"
 	"github.com/LerianStudio/midaz/v3/pkg/utils"
 	"github.com/google/uuid"
@@ -23,6 +27,14 @@ import (
 	libLog "github.com/LerianStudio/lib-commons/v4/commons/log"
 )
 
+// cacheNotFoundSentinel is the sentinel value stored in Redis when a transaction route is not found in the database.
+// This prevents repeated DB lookups for non-existent routes (negative caching).
+var cacheNotFoundSentinel = []byte("NOT_FOUND")
+
+// sentinelTTL is the expiration time for not-found sentinel entries in Redis.
+// NOTE: SetBytes multiplies this value by time.Second internally, so 60 means 60 seconds.
+const sentinelTTL = time.Duration(60)
+
 func (uc *UseCase) GetOrCreateTransactionRouteCache(ctx context.Context, organizationID, ledgerID, transactionRouteID uuid.UUID) (mmodel.TransactionRouteCache, error) {
 	logger, tracer, _, _ := libCommons.NewTrackingFromContext(ctx)
 
@@ -32,34 +44,49 @@ func (uc *UseCase) GetOrCreateTransactionRouteCache(ctx context.Context, organiz
 	internalKey := utils.AccountingRoutesInternalKey(organizationID, ledgerID, transactionRouteID)
 
 	cachedValue, err := uc.RedisRepo.GetBytes(ctx, internalKey)
-	if err != nil && err != redis.Nil {
+	if err != nil && !errors.Is(err, redis.Nil) {
 		logger.Log(ctx, libLog.LevelWarn, fmt.Sprintf("Error retrieving binary transaction route from cache: %v", err.Error()))
 	}
 
 	if err == nil && len(cachedValue) > 0 {
+		if bytes.Equal(cachedValue, cacheNotFoundSentinel) {
+			logger.Infof("Cache hit: not-found sentinel for transaction route %s", transactionRouteID)
+
+			libOpentelemetry.HandleSpanBusinessErrorEvent(&span, "Transaction route not found (sentinel cache hit)", services.ErrDatabaseItemNotFound)
+
+			return mmodel.TransactionRouteCache{}, services.ErrDatabaseItemNotFound
+		}
+
 		var cacheData mmodel.TransactionRouteCache
 
 		if err := cacheData.FromMsgpack(cachedValue); err != nil {
-			libOpentelemetry.HandleSpanError(span, "Failed to decode msgpack cache data", err)
+			logger.Log(ctx, libLog.LevelWarn, fmt.Sprintf("Corrupted cache data for transaction route %s, falling back to database: %v", transactionRouteID, err))
 
-			logger.Log(ctx, libLog.LevelError, fmt.Sprintf("Failed to decode msgpack cache data: %v", err))
-
-			return mmodel.TransactionRouteCache{}, err
+			libOpentelemetry.HandleSpanError(span, "Corrupted cache data, falling back to database", err)
+		} else {
+			return cacheData, nil
 		}
-
-		return cacheData, nil
 	}
 
 	foundTransactionRoute, err := uc.TransactionRouteRepo.FindByID(ctx, organizationID, ledgerID, transactionRouteID)
 	if err != nil {
-		if err == services.ErrDatabaseItemNotFound {
+		var entityNotFound pkg.EntityNotFoundError
+
+		isNotFound := errors.Is(err, services.ErrDatabaseItemNotFound) || errors.As(err, &entityNotFound)
+		if isNotFound {
 			msg := "Transaction route not found in database"
 
 			libOpentelemetry.HandleSpanBusinessErrorEvent(span, msg, err)
 
 			logger.Log(ctx, libLog.LevelWarn, msg)
 
-			return mmodel.TransactionRouteCache{}, err
+			if setErr := uc.RedisRepo.SetBytes(ctx, internalKey, cacheNotFoundSentinel, sentinelTTL); setErr != nil {
+				logger.Warnf("Failed to store not-found sentinel in cache: %v", setErr)
+			} else {
+				logger.Infof("Stored not-found sentinel for transaction route %s with TTL %ds", transactionRouteID, sentinelTTL)
+			}
+
+			return mmodel.TransactionRouteCache{}, services.ErrDatabaseItemNotFound
 		}
 
 		libOpentelemetry.HandleSpanError(span, "Failed to fetch transaction route from database", err)
