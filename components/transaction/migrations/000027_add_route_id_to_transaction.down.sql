a91a127b1dee644997092eac5b407dec13797ea2
Clara Tersi <mclara.tersi@gmail.com>
2026-03-20 08:54:14 -0300

    refactor: split index creation into separate migration for CONCURRENTLY :hammer:

--- components/transaction/migrations/000027_add_route_id_to_transaction.down.sql
+++ components/transaction/migrations/000027_add_route_id_to_transaction.down.sql
@@ -1,3 +1,2 @@
-DROP INDEX IF EXISTS idx_transaction_route_id;
 ALTER TABLE "transaction" DROP CONSTRAINT IF EXISTS fk_transaction_route_id;
 ALTER TABLE "transaction" DROP COLUMN IF EXISTS route_id;
---
+++ components/transaction/migrations/000028_create_idx_transaction_route_id.down.sql
@@ -0,0 +1,1 @@
+DROP INDEX IF EXISTS idx_transaction_route_id;
