f4e0bdab1cc7cf154f2c2dc3c56584a582a9044c
Clara Tersi <mclara.tersi@gmail.com>
2026-04-06 20:02:22 -0300

    refactor: create organization :hammer:

--- components/ledger/internal/services/command/create_onboarding_metadata.go
+++ components/ledger/internal/services/command/create_onboarding_metadata.go
@@ -6,38 +6,42 @@ package command
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	libCommons "github.com/LerianStudio/lib-commons/v4/commons"
 	libLog "github.com/LerianStudio/lib-commons/v4/commons/log"
+	libOpentelemetry "github.com/LerianStudio/lib-commons/v4/commons/opentelemetry"
 	mongodb "github.com/LerianStudio/midaz/v3/components/ledger/internal/adapters/mongodb/onboarding"
 )
 
+// CreateOnboardingMetadata persists the given metadata in MongoDB for the specified entity.
+// If metadata is nil, no document is created and (nil, nil) is returned.
 func (uc *UseCase) CreateOnboardingMetadata(ctx context.Context, entityName, entityID string, metadata map[string]any) (map[string]any, error) {
 	logger, tracer, _, _ := libCommons.NewTrackingFromContext(ctx)
-
-	logger.Log(ctx, libLog.LevelInfo, fmt.Sprintf("Trying to create metadata for %s: %v", entityName, entityID))
 
 	ctx, span := tracer.Start(ctx, "command.create_metadata")
 	defer span.End()
 
-	if metadata != nil {
-		meta := mongodb.Metadata{
-			EntityID:   entityID,
-			EntityName: entityName,
-			Data:       metadata,
-			CreatedAt:  time.Now(),
-			UpdatedAt:  time.Now(),
-		}
-
-		if err := uc.OnboardingMetadataRepo.Create(ctx, entityName, &meta); err != nil {
-			logger.Log(ctx, libLog.LevelError, fmt.Sprintf("Error into creating %s metadata: %v", entityName, err))
-			return nil, err
-		}
-
-		return metadata, nil
+	if metadata == nil {
+		return nil, nil
 	}
 
-	return nil, nil
+	now := time.Now()
+
+	meta := mongodb.Metadata{
+		EntityID:   entityID,
+		EntityName: entityName,
+		Data:       metadata,
+		CreatedAt:  now,
+		UpdatedAt:  now,
+	}
+
+	if err := uc.OnboardingMetadataRepo.Create(ctx, entityName, &meta); err != nil {
+		libOpentelemetry.HandleSpanBusinessErrorEvent(span, "Failed to create metadata", err)
+		logger.Log(ctx, libLog.LevelError, "Failed to create metadata", libLog.Err(err))
+
+		return nil, err
+	}
+
+	return metadata, nil
 }
