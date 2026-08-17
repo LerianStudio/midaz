c7b53d94247cf3dba20f5d66386bdcd23588f80f
Fred Amaral <fred@fredamaral.com.br>
2026-08-17 17:42:34 +0000

    test: give RabbitMQ a viable memory budget

--- tests/utils/rabbitmq/container.go
+++ tests/utils/rabbitmq/container.go
@@ -50,7 +50,7 @@ func DefaultContainerConfig() ContainerConfig {
 		User:     DefaultUser,
 		Password: DefaultPassword,
 		Image:    "rabbitmq:4.1-management-alpine",
-		MemoryMB: 256, // 256MB - moderate for messaging
+		MemoryMB: 512,
 		CPULimit: 0.5, // 0.5 CPU core
 	}
 }
